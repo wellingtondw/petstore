55f02d0029d1077b9803a1ee1c6773cefd858625
wellington <wellingtonslpbi@hotmail.com>
2021-02-04 22:41:50 -0300

    add bootstrap and font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignUp from './pages/SignUp'
 
+import './styles/global.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <SignUp />
